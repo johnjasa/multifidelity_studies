f9ed6e32de23eecd135fdefe71caf44f2ad14276
John Jasa <johnjasa11@gmail.com>
2020-07-14 13:57:36 -0600

    Moved trust region to its own file

--- multifidelity_studies/methods/base_method.py
+++ multifidelity_studies/methods/base_method.py
@@ -5,7 +5,6 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 import smt.surrogate_models as smt
-from multifidelity_studies.models.testbed_components import simple_2D_high_model, simple_2D_low_model
 
 
 class BaseMethod():
@@ -17,8 +16,8 @@ class BaseMethod():
         
         desvars = self.list_of_desvars[-1]
         
-        self.model_low = simple_2D_low_model(desvars, 'low_2D_results.pkl')
-        self.model_high = simple_2D_high_model(desvars, 'high_2D_results.pkl')
+        self.model_low = model_low(desvars, 'low_2D_results.pkl')
+        self.model_high = model_high(desvars, 'high_2D_results.pkl')
         
     def construct_approximation(self, interp_method='smt'):
         y_low = self.model_low.run_vec(self.list_of_desvars)
@@ -86,8 +85,8 @@ class BaseMethod():
             list_of_desvars_plot.append(desvars)
             surrogate[j] = self.approximation_function(x_values[j])
             
-        # y_plot_high = self.model_high.run_vec(list_of_desvars_plot).reshape(n_plot, n_plot)
-        y_plot_high = surrogate.reshape(n_plot, n_plot)
+        y_plot_high = self.model_high.run_vec(list_of_desvars_plot).reshape(n_plot, n_plot)
+        # y_plot_high = surrogate.reshape(n_plot, n_plot)
     
         plt.figure()
         plt.contourf(X, Y, y_plot_high, levels=101)
@@ -110,88 +109,3 @@ class BaseMethod():
         plt.show()
         
         
-class SimpleTrustRegion(BaseMethod):
-    
-    def __init__(self, model_low, model_high, max_trust_radius=1000., eta=0.15, gtol=1e-4, trust_radius=0.2):
-        super().__init__(model_low, model_high)
-        
-        self.max_trust_radius = max_trust_radius
-        self.eta = eta
-        self.gtol = gtol
-        self.trust_radius = trust_radius
-        
-    def find_next_point(self):
-        x0 = self.x[-1, :]
-        
-        # min (m_k(x_k + s_k)) st ||x_k|| <= del K
-        lower_bounds = x0 - self.trust_radius
-        lower_bounds[lower_bounds < 0.] = 0.
-        upper_bounds = x0 + self.trust_radius
-        upper_bounds[upper_bounds > 1.] = 1.
-        
-        bounds = list(zip(lower_bounds, upper_bounds))
-        res = minimize(self.approximation_function, x0, method='SLSQP', tol=1e-10, bounds=bounds)
-        x_new = res.x
-        
-        if np.any(np.abs(lower_bounds - x_new) < 1e-6) or np.any(np.abs(upper_bounds - x_new) < 1e-6):
-            hits_boundary = True
-        else:
-            hits_boundary = False
-            
-        return x_new, hits_boundary
-    
-    def update_trust_region(self, x_new, hits_boundary):
-        # 3. Compute the ratio of actual improvement to predicted improvement
-        desvars = OrderedDict()
-        desvars['x'] = np.squeeze(x_new)
-        
-        actual_reduction = self.model_high.run(self.list_of_desvars[-1]) - self.model_high.run(desvars)
-        predicted_reduction = self.model_high.run(self.list_of_desvars[-1]) - self.approximation_function(x_new)
-        
-        # 4. Accept or reject the trial point according to that ratio
-        if predicted_reduction <= 0:
-            print('not enough reduction! rejecting point')
-        else:
-            self.x = np.vstack((self.x, np.atleast_2d(x_new)))
-            self.list_of_desvars.append(desvars)
-            
-        if predicted_reduction == 0.:
-            rho = 0.
-        else:
-            rho = actual_reduction / predicted_reduction
-    
-        # 5. Update trust region according to rho_k
-        eta = 0.25
-        if rho < eta:
-            self.trust_radius *= 0.25
-        elif rho > eta:  #0.75 and hits_boundary:
-            self.trust_radius = min(2*self.trust_radius, self.max_trust_radius)
-        print('trust radius', self.trust_radius)
-            
-    def optimize(self):
-        self.construct_approximation()
-        self.plot_functions()
-        
-        for i in range(20):
-            x_new, hits_boundary = self.find_next_point()
-            
-            self.update_trust_region(x_new, hits_boundary)
-                
-            print(self.x)
-            print(self.model_high.run_vec(self.list_of_desvars))
-            print()
-            
-            self.construct_approximation()
-        
-            self.plot_functions()
-            
-            if self.trust_radius <= 1e-5:
-                print("Found optimal point!")
-        
-
-# Following Algo 2.1 from Andrew March's dissertation
-np.random.seed(13)
-
-trust_region = SimpleTrustRegion(simple_2D_low_model, simple_2D_high_model)
-trust_region.optimize()
-
--- multifidelity_studies/methods/trust_region.py
+++ multifidelity_studies/methods/trust_region.py
@@ -1,98 +1,97 @@
+# base method class
 import numpy as np
 from scipy.interpolate import Rbf
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
-from multifidelity_studies.models.testbed_components import simple_1D_low, simple_1D_high
+from collections import OrderedDict
+import smt.surrogate_models as smt
+from multifidelity_studies.models.testbed_components import simple_2D_high_model, simple_2D_low_model
+from multifidelity_studies.methods.base_method import BaseMethod
 
 
-func_low = simple_1D_low
-func_high = simple_1D_high
+class SimpleTrustRegion(BaseMethod):
+    
+    def __init__(self, model_low, model_high, max_trust_radius=1000., eta=0.15, gtol=1e-4, trust_radius=0.2):
+        super().__init__(model_low, model_high)
+        
+        self.max_trust_radius = max_trust_radius
+        self.eta = eta
+        self.gtol = gtol
+        self.trust_radius = trust_radius
+        
+    def find_next_point(self):
+        x0 = self.x[-1, :]
+        
+        # min (m_k(x_k + s_k)) st ||x_k|| <= del K
+        lower_bounds = x0 - self.trust_radius
+        lower_bounds[lower_bounds < 0.] = 0.
+        upper_bounds = x0 + self.trust_radius
+        upper_bounds[upper_bounds > 1.] = 1.
+        
+        bounds = list(zip(lower_bounds, upper_bounds))
+        res = minimize(self.approximation_function, x0, method='SLSQP', tol=1e-10, bounds=bounds)
+        x_new = res.x
+        
+        if np.any(np.abs(lower_bounds - x_new) < 1e-6) or np.any(np.abs(upper_bounds - x_new) < 1e-6):
+            hits_boundary = True
+        else:
+            hits_boundary = False
+            
+        return x_new, hits_boundary
+    
+    def update_trust_region(self, x_new, hits_boundary):
+        # 3. Compute the ratio of actual improvement to predicted improvement
+        desvars = OrderedDict()
+        desvars['x'] = np.squeeze(x_new)
+        
+        actual_reduction = self.model_high.run(self.list_of_desvars[-1]) - self.model_high.run(desvars)
+        predicted_reduction = self.model_high.run(self.list_of_desvars[-1]) - self.approximation_function(x_new)
+        
+        # 4. Accept or reject the trial point according to that ratio
+        if predicted_reduction <= 0:
+            print('not enough reduction! rejecting point')
+        else:
+            self.x = np.vstack((self.x, np.atleast_2d(x_new)))
+            self.list_of_desvars.append(desvars)
+            
+        if predicted_reduction == 0.:
+            rho = 0.
+        else:
+            rho = actual_reduction / predicted_reduction
+    
+        # 5. Update trust region according to rho_k
+        eta = 0.25
+        if rho < eta:
+            self.trust_radius *= 0.25
+        elif rho > eta and hits_boundary:
+            self.trust_radius = min(2*self.trust_radius, self.max_trust_radius)
+        print('trust radius', self.trust_radius)
+            
+    def optimize(self):
+        self.construct_approximation()
+        self.plot_functions()
+        
+        for i in range(20):
+            x_new, hits_boundary = self.find_next_point()
+            
+            self.update_trust_region(x_new, hits_boundary)
+                
+            print(self.x)
+            print(self.model_high.run_vec(self.list_of_desvars))
+            print()
+            
+            self.construct_approximation()
+        
+            self.plot_functions()
+            
+            if self.trust_radius <= 1e-5:
+                print("Found optimal point!")
+                break
+        
 
 # Following Algo 2.1 from Andrew March's dissertation
-np.random.seed(3)
+np.random.seed(13)
 
-# Misc utilities
-def plot_functions():
-    x_plot = np.linspace(0., 1., 101)
-    surrogate = m(x_plot)
-    y_plot_low = func_low(x_plot)
-    y_plot_high = func_high(x_plot)
+trust_region = SimpleTrustRegion(simple_2D_low_model, simple_2D_high_model)
+trust_region.optimize()
 
-    plt.figure()
-    plt.plot(x_plot, y_plot_low, label='low')
-    plt.plot(x_plot, y_plot_high, label='high')
-    plt.plot(x_plot, surrogate, label='mixed surrogate')
-    plt.scatter(x, y_high, color='k')
-    plt.legend()
-    plt.show()
-
-max_trust_radius = 1000.0
-eta = 0.15
-gtol = 1e-4
-trust_radius = 0.1
-
-# 1. Compute a step, s_k, that satisfies the fraction of Cauchy decrease requirement by solving:
-
-# Query hifi and lofi in same places
-x = np.random.rand(3)
-y_low = func_low(x)
-y_high = func_high(x)
-
-# Construct RBF interpolater for error function
-differences = y_high - y_low
-e = Rbf(x, differences)
-
-# Create m_k = lofi + RBF
-m = lambda x: simple_1D_low(x) + e(x)
-
-# plot_functions()
-
-for i in range(10):
-    # min (m_k(x_k + s_k)) st ||x_k|| <= del K
-    x0 = x[-1]
-    bounds = [(max(x0 - trust_radius, 0.), min(1., x0 + trust_radius))]
-    res = minimize(m, x0, method='SLSQP', tol=1e-6, bounds=bounds)
-    x_new = res.x
-    
-    bounds = np.array(bounds)
-    lower_bounds = bounds[:, 0]
-    upper_bounds = bounds[:, 1]
-    
-    if np.any(np.abs(lower_bounds - x_new) < 1e-6) or np.any(np.abs(upper_bounds - x_new) < 1e-6):
-        hits_boundary = True
-    else:
-        hits_boundary = False
-    
-    x = np.hstack((x, x_new))
-    y_low = func_low(x)
-    
-    # 2. If f_high(x + x_k) hasn't been evaluated, do it and save it
-    y_high = func_high(x)
-    
-    # 3. Compute the ratio of actual improvement to predicted improvement
-    actual_reduction = func_high(x[-2]) - func_high(x_new)
-    predicted_reduction = func_high(x[-2]) - m(x_new)
-    
-    # 4. Accept or reject the trial point according to that ratio
-    if predicted_reduction <= 0:
-        print('not enough reduction! probably at a local optimum')
-        break
-        
-    rho = actual_reduction / predicted_reduction
-
-    # 5. Update trust region according to rho_k
-    if rho < 0.25:
-        trust_radius *= 0.25
-    elif rho > 0.75 and hits_boundary:
-        trust_radius = min(2*trust_radius, max_trust_radius)
-
-    # 6. Create a new model m_k+1 using Algo 2.2
-    differences = y_high - y_low
-    e = Rbf(x, differences)
-
-    # Create m_k = lofi + RBF
-    m = lambda x: simple_1D_low(x) + e(x)
-
-    print(x)
-
-    plot_functions()
