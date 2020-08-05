225ea98177967154674fad11072f916baf5c61a9
Bortolotti <ptrbortolotti@gmail.com>
2020-08-04 18:40:28 -0600

    code updated to IEAontology4all

--- multifidelity_studies/models/run.py
+++ multifidelity_studies/models/run.py
@@ -7,17 +7,17 @@ import os
 run_dir = os.path.dirname(os.path.realpath(__file__)) + os.sep
 # fname_wt_input         = run_dir + 'IEAonshoreWT_3.yaml'
 fname_wt_input = run_dir + "IEA-15-240-RWT_WISDEMieaontology4all.yaml"
-fname_analysis_options_ccblade = run_dir + "analysis_options_ccblade.yaml"
-fname_analysis_options_openfast = run_dir + "analysis_options_openfast.yaml"
-fname_opt_options = run_dir + "optimization_options.yaml"
+fname_modeling_options_ccblade = run_dir  + "modeling_options_ccblade.yaml"
+fname_modeling_options_openfast = run_dir + "modeling_options_openfast.yaml"
+fname_analysis_options = run_dir + "analysis_options.yaml"
 folder_output = run_dir + "it_0/"
 fname_wt_output = folder_output + "temp.yaml"
 
 # Run CCBlade
 wt_opt_ccblade, analysis_options_ccblade, opt_options_ccblade = run_wisdem(
     fname_wt_input,
-    fname_analysis_options_ccblade,
-    fname_opt_options,
+    fname_modeling_options_ccblade,
+    fname_analysis_options,
     fname_wt_output,
     folder_output,
 )
@@ -25,8 +25,8 @@ wt_opt_ccblade, analysis_options_ccblade, opt_options_ccblade = run_wisdem(
 # Run OpenFAST
 wt_opt_openfast, analysis_options_openfast, opt_options_openfast = run_wisdem(
     fname_wt_input,
-    fname_analysis_options_openfast,
-    fname_opt_options,
+    fname_modeling_options_openfast,
+    fname_analysis_options,
     fname_wt_output,
     folder_output,
 )
@@ -38,4 +38,4 @@ else:
 
 if rank == 0:
     print("Aerodynamic Cp CCblade    = " + str(wt_opt_ccblade["ccblade.CP"]))
-    print("Aerodynamic Cp OpenFAST   = " + str(wt_opt_openfast["aeroelastic.Cp"]))
+    print("Aerodynamic Cp OpenFAST   = " + str(wt_opt_openfast["aeroelastic.Cp_out"]))
